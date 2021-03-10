e2c52b5dd26b99ae13983166d4bdd00f50b29b23
ShookLyngs <shook-lyngs@qq.com>
2021-03-11 00:59:03 +0800

    update(types): update types file.

---
+++ lib/themer-react/types/hook.d.ts
@@ -0,0 +1,3 @@
+import { ThemeManager, ThemeManagerOptions } from '@lyngs/themer';
+export declare function useThemeManager(key: string): ThemeManager;
+export declare function createThemeManager(options: ThemeManagerOptions): string;
